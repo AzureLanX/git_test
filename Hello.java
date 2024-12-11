fec0798d8787fd22466232ff01e76adcf41e4544
liuyun <2845611727@qq.com>
2024-12-11 22:49:26 +0800

    there a tset for github

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Author :WeiMin Zheng
+ * Date:2024/12/11
+ * Time:22:46
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
