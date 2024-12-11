72d4d9888058a616be888e276eabe95db9262546
liuyun <2845611727@qq.com>
2024-12-11 22:54:59 +0800

    diercitijiaozhege

---
+++ TestGithub.java
@@ -0,0 +1,10 @@
+/**
+ * Author :WeiMin Zheng
+ * Date:2024/12/11
+ * Time:22:54
+ */
+public class TestGithub {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+}
